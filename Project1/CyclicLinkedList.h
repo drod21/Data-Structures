cd0165183f326f8fcf55adc7eb4ce644d262345f
Derek Rodriguez <drod2169@gmail.com>
2017-05-17 16:31:57 -0400

    Implement the function definitions (some empty still, need to finish) required for the single linked list section. Still need to implement functions in double linked list.

--- Project1/CyclicLinkedList.h
+++ Project1/CyclicLinkedList.h
@@ -9,5 +9,100 @@
 #ifndef CyclicLinkedList_h
 #define CyclicLinkedList_h
 
+#include "SingleNode.h"
+
+using namespace std;
+
+typedef int ElemType;
+
+class CyclicLinkedList {
+    
+    private:
+    SingleNode *head;   // Pointer to the head of the list.
+    SingleNode *tail;   // Pointer to the tail of the list.
+    int n;  // Integer to store the size of the list.
+    
+    
+    public:
+    
+    // Constructor
+    CyclicLinkedList() {
+        
+        head = nullptr;
+        tail = nullptr;
+        n = 0;
+        
+    }
+    
+    int size() const {
+        return n;
+    }
+    bool empty() const {
+        //Return true if list is empty, false otherwise.
+        if (head != nullptr) {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    ElemType front() const {
+        // Retrieve the object stored in the node pointed to by the head pointer.
+        // Throw underflow if list is empty.
+        
+        if (empty()) {
+            
+            throw underflow_error;
+            
+        }
+        
+        return head->nodeData;
+        
+    }
+    
+    ElemType back() const {
+        
+        if (empty()) {
+            throw underflow_error;
+        }
+        
+        return tail->nodeData;
+    }
+    
+    // Returns head pointer
+    SingleNode<ElemType> *head() const {
+        return head;
+    }
+    
+    // Returns tail pointer
+    SingleNode<ElemType> *tail() const {
+        return tail;
+    }
+    
+    int count(ElemType const &) const {
+        // Returns the number of nodes in the linked list,
+        // storing a value equal to the argument.
+    }
+    
+    void push_front(ElemType const &) {
+        
+        
+    }
+    
+    void push_back(ElemType const &) {
+        
+    }
+    
+    ElemType pop_front() {
+        
+    }
+    
+    int erase(ElemType const &) {
+        
+        
+    }
+
+};
+
 
 #endif /* CyclicLinkedList_h */
--- Project1/SingleNode.h
+++ Project1/SingleNode.h
@@ -9,5 +9,39 @@
 #ifndef SingleNode_h
 #define SingleNode_h
 
+using namespace std;
+
+template <class T>
+
+class SingleNode {
+
+    // Public members. nodeData and *next are public because they'll be accessed in
+    // CyclicLinkedList class as private members
+    
+    public:
+    T nodeData;
+    SingleNode *next;
+    
+    // Constructor
+    
+    SingleNode(ElemType const &, SingleNode *) {
+        
+        
+        
+    }
+    // Accessors
+    
+    T getData() const {
+        return nodeData;        // Returns the data for the node.
+    }
+    
+    SingleNode *getNext() const {
+        return next;            // Returns a pointer to the next element.
+    }
+    
+    
+};
+
+friend class CyclicLinkedList<Type>;
 
 #endif /* SingleNode_h */
