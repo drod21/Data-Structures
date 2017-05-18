56d989319a3c512622424438c6e5902e5d7f4591
derek29 <caprio_derek@yahoo.com>
2017-05-18 17:19:54 -0400

    DoubleNode.h file completed

--- Project1/DoubleNode.h
+++ Project1/DoubleNode.h
@@ -8,6 +8,44 @@
 
 #ifndef DoubleNode_h
 #define DoubleNode_h
+#include "DoublyLinkedList.h"
+#include <iostream>
+using namespace std;
 
+// template for data type
+template <class T>;
+
+// DoubleNode class definition
+class DoubleNode(){
+    // a DoublyLinkedList has a DoubleNode
+    friend DoublyLinkedList;
+    // private data members
+    private:
+            T data;
+            DoubleNode* next;
+            DoubleNode* previous;
+    public:
+        // constructor. usage: DoubleNode(data, previous, next)
+        DoubleNode(T const& d, DoubleNode* p, DoubleNode* n){
+            data = d;
+            previous = p
+            next = n
+        }
+        // destructor
+        ~DoubleNode();
+        // get data function returns data stored at node
+        T getData(void) const {
+            return data;
+        }
+        // getNext function returns pointer to next element
+        DoubleNode* getNext(void) const {
+            return next;
+        }
+        // getPrevious function returns pointer to previous element
+        DoubleNode* getPrevious(void) const {
+            return previous;
+        }
+}
 
 #endif /* DoubleNode_h */
+
