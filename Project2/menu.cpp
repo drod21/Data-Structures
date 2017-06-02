bcc2a3adf17f3c308b1b5314039c28f63573dcd1
Derek Rodriguez <drod2169@gmail.com>
2017-06-02 08:37:55 -0400

    Add queue options to menu

--- Project2/menu.cpp
+++ Project2/menu.cpp
@@ -19,16 +19,16 @@ int main() {
     
     char ch;
     int menu_number = 0,
-        size,
+    size,
     capacity;
     
     Type top,
-        removed,
-        toAdd;
+    removed,
+    toAdd;
     
     // Decreased for ease of testing
     DynStack<Type> stack(3);
-    DynQueue<Type> queue;
+    DynQueue<Type> queue(3);
     
     
     cout << "Please enter 's' for stack, 'q' for queue: ";
@@ -40,103 +40,118 @@ int main() {
     if (ch == 's') {
         while (menu_number != 8) {
             
-            cout << "1. Return Capacity (items actually in the data structure) " << endl;
+            cout << "1. Return Capacity" << endl;
             cout << "2. Return Size of the data structure" << endl;
-            cout << "3. View first item ('top' for Stack, 'front' for Queue)" << endl;
-            cout << "4. Insert item ('push' for Stack, 'enqueue' for Queue)" << endl;
-            cout << "5. Delete item ('pop' for Stack, 'dequeue' for Queue)" << endl;
+            cout << "3. View first item" << endl;
+            cout << "4. Insert item" << endl;
+            cout << "5. Delete item" << endl;
             cout << "6. Display" << endl;
             cout << "7. Clear" << endl;
             cout << "8. Exit" << endl;
             
             cin >> menu_number;
-        switch (menu_number) {
-            
-            case 1:
-                size = stack.size();
-                cout << "Capacity of the stack: " << size << endl;
-                break;
-            
-            case 2:
-                capacity = stack.capacity();
-                cout << "Size of the data structure: " << capacity << endl;
-                break;
-            
-            case 3:
-                top = stack.top();
-                cout << "First item of the stack: " << top << endl;
-                break;
-            
-            case 4:
-                cout << "Please enter an item to insert: ";
-                cin.ignore();
-                getline(cin, toAdd);
-                stack.push(toAdd);
-                break;
-            
-            case 5:
-                removed = stack.pop();
-                cout << "Item " << removed << " popped from the stack." << endl;
-                break;
-            
-            case 6:
-                stack.display();
-                break;
-            
-            case 7:
-                stack.clear();
-                break;
-            case 8:
-                return 0;
-                break;
-            
-            default:
-                cout << "Invalid selection, please make another selection: ";
-                cin >> menu_number;
-                cout << endl;
-        }
+            switch (menu_number) {
+                    
+                case 1:
+                    size = stack.size();
+                    cout << "Capacity of the stack: " << size << endl;
+                    break;
+                    
+                case 2:
+                    capacity = stack.capacity();
+                    cout << "Size of the data structure: " << capacity << endl;
+                    break;
+                    
+                case 3:
+                    top = stack.top();
+                    cout << "First item of the stack: " << top << endl;
+                    break;
+                    
+                case 4:
+                    cout << "Please enter an item to insert: ";
+                    cin.ignore();
+                    getline(cin, toAdd);
+                    stack.push(toAdd);
+                    break;
+                    
+                case 5:
+                    removed = stack.pop();
+                    cout << "Item " << removed << " popped from the stack." << endl;
+                    break;
+                    
+                case 6:
+                    stack.display();
+                    break;
+                    
+                case 7:
+                    stack.clear();
+                    break;
+                case 8:
+                    return 0;
+                    break;
+                    
+                default:
+                    cout << "Invalid selection, please make another selection: ";
+                    cin >> menu_number;
+                    cout << endl;
+            }
         }
         
     } else if (ch == 'q') {
         while (menu_number != 8) {
+            cout << "1. Return Capacity" << endl;
+            cout << "2. Return Size of the data structure" << endl;
+            cout << "3. View first item" << endl;
+            cout << "4. Insert item" << endl;
+            cout << "5. Delete item" << endl;
+            cout << "6. Display" << endl;
+            cout << "7. Clear" << endl;
+            cout << "8. Exit" << endl;
+            
+            cin >> menu_number;
             switch (menu_number) {
-                
-            case 1:
-                
-                break;
-                
-            case 2:
-                
-                break;
-                
-            case 3:
-                
-                break;
-                
-            case 4:
-                
-                break;
-                
-            case 5:
-                
-                break;
-                
-            case 6:
-                
-                break;
-                
-            case 7:
-                
-                break;
+                case 1:
+                    size = queue.size();
+                    cout << "Capacity of the queue: " << size << endl;
+                    break;
                     
-            case 8:
-                return 0;
-                break;
-                
-            default:
-                cout << "Invalid selection, please make another selection: ";
-                cin >> menu_number;
-                cout << endl;
+                case 2:
+                    capacity = queue.capacity();
+                    cout << "Size of the data structure: " << capacity << endl;
+                    break;
+                    
+                case 3:
+                    top = queue.front();
+                    cout << "First item of the queue: " << top << endl;
+                    break;
+                    
+                case 4:
+                    cout << "Please enter an item to insert: ";
+                    cin.ignore();
+                    getline(cin, toAdd);
+                    queue.enqueue(toAdd);
+                    break;
+                    
+                case 5:
+                    removed = queue.dequeue();
+                    cout << "Item " << removed << " removed from the queue." << endl;
+                    break;
+                    
+                case 6:
+                    queue.display();
+                    break;
+                    
+                case 7:
+                    queue.clear();
+                    break;
+                case 8:
+                    return 0;
+                    break;
+                    
+                default:
+                    cout << "Invalid selection, please make another selection: ";
+                    cin >> menu_number;
+                    cout << endl;
             }
         }
     }
