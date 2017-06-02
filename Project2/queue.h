7d5a6b57b2fd2d28335b10787a1b54ad3da1f7c0
Derek Rodriguez <drod2169@gmail.com>
2017-06-02 08:38:48 -0400

    Queue updates
    Added copy constructed & overloaded assignment operator. Enqueue seems
    to work properly, and so does dequeue, but display isn’t properly
    displaying after dequeue.

--- Project2/queue.h
+++ Project2/queue.h
@@ -9,24 +9,25 @@
 #ifndef queue_h
 #define queue_h
 
-
+#include <string>
 template <class Type>
+using namespace std;
 
 class DynQueue {
-
+    
 private:
     
     Type *array; // To be used as an array
     
     int iHead, // Head index
-        iTail, // Tail index
-        count, // Counter
-        initialSize, // Initial size of the array
-        arraySize; // Current size of the array
+    iTail, // Tail index
+    count, // Counter
+    initialSize, // Initial size of the array
+    arraySize; // Current size of the array
     
 public:
     // Constructor
-    DynQueue(int n = 15) {
+    DynQueue(int n = 15) : arraySize(n), count(0), iHead(0), iTail(0) {
         // Test values to set initial size
         if (n <= 0) {
             initialSize = 1;
@@ -36,7 +37,22 @@ public:
         // Allocate memory
         array = new Type[initialSize];
     }
-    
+    // Copy Constructor
+    DynQueue(const DynQueue &queue):initialSize(queue.initialSize),
+    arraySize(queue.arraySize), count(queue.count),
+    iHead(queue.iHead), iTail(queue.iTail) {
+        
+        array = new (nothrow) Type[initialSize];
+        
+        if (array != 0) {
+            for (int i = 0; i < initialSize; i++) {
+                array[i] = queue.array[i];
+            }
+        } else {
+            cerr << "Cannot allocate memory";
+            exit(1);
+        }
+    }
     // Destructor
     ~DynQueue() {
         if (!empty())
@@ -66,7 +82,7 @@ public:
     
     int size() const {
         
-        return arraySize;
+        return count;
         
     }
     
@@ -76,37 +92,74 @@ public:
     
     int capacity() const {
         
-        return initialSize;
+        return arraySize;
         
     }
     void display() {
-        
+        cout << "Contents of the queue: " << endl;
+        for (int i = count; i >= 0; i--) {
+            cout << array[i] << endl;
+        }
+        cout << endl;
         
     }
     
     // Mutators
     
     void enqueue(Type const &data) {
-        Type *a = nullptr;
-        if (size() == capacity()) {
-            initialSize = initialSize * 2;
-            a = new Type[initialSize];
-            for (count = 0; count < arraySize; count++) {
-                a[count] = array[count];
+        
+        if (size() != capacity()) {
+            array[iTail] = data;
+            iTail = (iTail + 1) % arraySize;
+            ++count;
+        } else {
+            arraySize = arraySize * 2;
+            Type *a = new Type[arraySize];
+            for(int i = 0; i <= count; i++){
+                a[i] = array[i];
             }
+            delete [] array;
             array = a;
+            array[iTail] = data;
+            iTail = (iTail + 1) % arraySize;
+            ++count;
         }
         
-        //array[iHead];
     }
     
     
     Type dequeue() throw(std::underflow_error) {
+        Type removed = array[iHead];
+        if (empty()) {
+            throw underflow_error("Queue is empty");
+        }
         
+        iHead = (iHead + 1) % arraySize;
+        --count;
+        return removed;
     }
     
     void clear() {
         
+    }
+    
+    // Overload assignment operator
+    DynQueue &operator=(const DynQueue &queue) {
+        if (this != &queue) {
+            arraySize = queue.arraySize;
+            initialSize = queue.initialSize;
+            iHead = queue.iHead;
+            iTail = queue.iTail;
+            count = queue.count;
+            if (initialSize != queue.initialSize) {
+                delete [] array;
+                array = new(nothrow) Type[initialSize];
+            }
+            for (int i = 0; i < initialSize; i++) {
+                array[i] = queue.array[i];
+            }
+        }
+        return *this;
     }
     
     
