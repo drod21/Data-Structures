8ba056c786ae346824fcf304267be5d3ced82919
Derek Rodriguez <drod2169@gmail.com>
2017-06-27 08:14:22 -0400

    Fix compile warning, compile errors

--- Project3/menu.cpp
+++ Project3/menu.cpp
@@ -11,6 +11,7 @@
 #include "linkedTree.h"
 #include "avlTree.h"
 
+
 int main() {
     typedef string Node;
     
@@ -18,7 +19,6 @@ int main() {
 
     int keyToInsert;
     Node dataToInsert;
-    Node *nodeToGetHeight;
     Node dataToDelete;
     char tree_type;
     int menu_number = 0;
@@ -184,7 +184,6 @@ int main() {
             }// end switch
         } // end while
     }
-    
     if (tree_type == 'a') {
         
         while (menu_number != 15) {
@@ -207,94 +206,140 @@ int main() {
             
             cin >> menu_number;
             
-            /* switch (menu_number) {
-             
-             case 1:
-             cout << "Root: " << avlTree->getRoot() << endl;
-             break;
-             
-             case 2:
-             cout << "Size: " << avlTree->getSize() << endl;
-             break;
-             
-             case 3:
-             cout << "Height of tree: " << avlTree->getHeight() << endl;
-             break;
-             
-             case 4:
-             cout << "Find height of which node?: ";
-             cin >> data;
-             nodeToGetHeight = avlTree->find(data);
-             cout << "Height of node: " << avlTree->getHeight(nodeToGetHeight) << endl;
-             break;
-             
-             case 5:
-             if(avlTree->empty() == true)
-             cout << "Tree is empty" << endl;
-             else
-             cout << "Tree is not empty" << endl;
-             break;
-             
-             case 6:
-             cout << "Number of leaves: " << avlTree->getLeaves() << endl;
-             break;
-             
-             case 7:
-             cout << "Get number of siblings for whcih node?: ";
-             cin >> data;
-             Node nodeToGetSiblings = avlTree->find(data);
-             cout << "Number of node's siblings: " << avlTree->siblings(nodeToGetSiblings) << endl;
-             break;
-             
-             case 8:
-             cout << "Find which node?: ";
-             cin >> data;
-             Node nodeToFind = avlTree->find(data);
-             cout << "Found node: " << nodeToFind << endl;
-             break;
-             
-             case 9:
-             cout << "Preorder: " << avlTree->preorder() << endl;
-             break;
-             
-             case 10:
-             cout << "Postorder: " << avlTree->postorder() << endl;
-             break;
-             
-             case 11:
-             cout << "Inorder: " << avlTree->inorder() << endl;
-             break;
-             
-             case 12:
-             avlTree->clear();
-             cout << "Tree cleared" << endl;
-             break;
-             
-             case 13:
-             
-             cout << "Enter data to insert: ";
-             cin >> dataToInsert;
-             avlTree->insert(dataToInsert);
-             cout << endl;
-             break;
-             
-             case 14:
-             Node dataToDelete;
-             cout << "Enter data to delete: ";
-             cin >> dataToDelete;
-             avlTree->del(dataToDelete);
-             break;
-             
-             case 15:
-             return 0;
-             break;
-             
-             default:
-             cout << "Invalid selection. Enter option [1-15]: ";
-             cin >> menu_number;
-             cout << endl;
-             break;
-             } // end switch*/
+            switch (menu_number) {
+                    
+                case 1:
+                    try {
+                        cout << "Root: " << avlTree->getRoot() << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 2:
+                    try {
+                        cout << "Size: " << avlTree->getSize() << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 3:
+                    try {
+                        cout << "Height of tree: " << avlTree->getHeight() << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 4:
+                    try {
+                        cout << "Find height of which node?: ";
+                        cin >> data;
+                        TreeNode<Node> *nodeToGetHeight = avlTree->find(data);
+                        cout << "Height of node: " << avlTree->getHeight(nodeToGetHeight) << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 5:
+                    if(avlTree->empty() == true) cout << "Tree is empty" << endl;
+                    else cout << "Tree is not empty" << endl;
+                    break;
+                    
+                case 6:
+                    try {
+                        cout << "Leaves: " << avlTree->leaves() << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 7:
+                    try {
+                        cout << "Find number of siblings for which node?: ";
+                        cin >> data;
+                        TreeNode<Node> *nodeToGetSiblings = avlTree->find(data);
+                        cout << "Number of node's siblings: " << avlTree->siblings(nodeToGetSiblings) << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 8:
+                    try {
+                        cout << "Find which node?: ";
+                        cin >> data;
+                        TreeNode<Node> *nodeToFind = avlTree->find(data);
+                        cout << "Found node: " << nodeToFind->getData() << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 9:
+                    try {
+                        cout << "Preorder: " << endl;
+                        avlTree->preorder();
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 10:
+                    try {
+                        cout << "Postorder: " << endl;
+                        avlTree->postorder();
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 11:
+                    try {
+                        cout << "Inorder: " << endl;
+                        avlTree->inorder();
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 12:
+                    avlTree->clear();
+                    cout << "Tree cleared" << endl;
+                    break;
+                    
+                case 13:
+                    cout << "Enter data to insert: ";
+                    cin >> dataToInsert;
+                    avlTree->insert(dataToInsert);
+                    cout << endl;
+                    break;
+                    
+                case 14:
+                    try {
+                        
+                        cout << "Enter data to delete: ";
+                        cin >> dataToDelete;
+                        avlTree->del(dataToDelete);
+                        cout << endl;
+                    } catch (underflow_error &e) {
+                        cout << e.what() << endl;
+                    }
+                    break;
+                    
+                case 15:
+                    return 0;
+                    break;
+                    
+                default:
+                    cout << "Invalid entry. Select an option [1-15]: ";
+                    cin >> menu_number;
+                    cout << endl;
+                    break;
+                    
+            }// end switch
         } // end while
     } else if (tree_type == 'h') {
         while (menu_number != 10) {
