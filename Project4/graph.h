c2e5adc64ce91c1c2645df7a8a5a66768cbe0204
derek29 <caprio_derek@yahoo.com>
2017-07-01 12:14:13 -0400

    some functions defined and some pseudo code written for graph.h

--- Project4/graph.h
+++ Project4/graph.h
@@ -10,12 +10,19 @@
 #include <list>
 #include <queue>
 #include <stack>
+#include <vector>
 
 #include "HashTable.h"
 #include "vertex.h"
 #include "edge.h"
 
 #define MAX_GRAPH_SIZE 20
+
+// adjacency list structure
+struct adjacencyList{
+	string vertexName;
+	string* next;
+};
 
 class Graph {
     
@@ -26,6 +33,7 @@ private:
     HashEntry **table;
     Vertex v;
     Edge e;
+	vector<adjacencyList*> adjListVector;
     
 public:
     
@@ -52,14 +60,18 @@ public:
     // returns true iff graph is empty
     bool empty(void) {
         bool isEmpty = false;
-        
+        if(adjListVector[0] == nullptr) isEmpty = true;
         return isEmpty;
-        
     }
     
     // returns degree of vertex v
     int degree(string v) {
         int deg = 0;
+		/* PSEUDO-CODE
+		while adjacency list at vector element v is not null
+			deg++
+			move to next item in adjacency list
+		*/
         
         return deg;
     }
@@ -68,7 +80,12 @@ public:
     int edgeCount(void) {
         
         int count = 0;
-        
+		/* PSEUDO-CODE
+		for each vertex in adjListVector
+			while adjacency list at adjListVector[i] is not null
+				count++
+				move to next item in adjacency list
+		*/
         
         return count;
     }
@@ -84,18 +101,52 @@ public:
     // returns weight of edge connecting vertices u and v
     double adjacent(string u, string v) {
         double w = 0.0;
-        
+		// if same vertex, return 0
+		if(u == v) return w;
+		/* PSEUDO-CODE
+			temp = u's position in adjListVector
+			while vertex at adjancyList[u]->next != v
+				tempVertex = currentVertex->next
+				if get to end of adjancyList return -1
+			return getEdge between u and currentVertex
+		*/
         return w;
     }
     
     // performs depth first search of graph starting at vertex v
+	// recursive
     void DFS(string v) {
-        
+        /* PSEUDO-CODE
+			reset graph so all vertices are uncolored
+			label v as visited
+			for all edges in v
+				if edge is unvisited
+					temp = vertex on other side of edge
+					if temp is uncolored
+						label edge as discovery edge
+						print vertex
+						recursively call DFS(temp)
+					else label edge as back edge
+		*/
     }
     
     // performs breadth first search of graph starting at vertex v
+	// non-recursive
     void BFS(string v) {
-        
+		/*
+        	initialize collectionn L0 to contain vertex v
+			int i = 0
+			for all vertices in Li
+				for all edges on vertex
+					if edge is unexplored
+						tempVertex = vertex on opposite side of edge
+						if tempVertex is unexplored
+							label edge as discovery edge
+							insert tempVertex into Li+1
+						else label edge as cross edge
+					print tempVertex
+				i++
+		*/
     }
     
     // uses Prim's algorithm to show minimum spanning tree of
@@ -115,14 +166,24 @@ public:
     }
     
     // marks all vertices as unvisited
-    void clearGraph(void) {
-        
+    void reset(void) {
+        /* PSEUDO-CODE
+			for each vertex in adjListVector
+				uncolor vertex
+				while currentVertex->next != null
+					currentVertex = currentVertex->next
+					uncolor vertex
+			print graph colring reset
+		*/
     }
     
     // inserts an edge with weight w between vertices u and v
     // if edge already exists, update weight with new w
     void insert(string u, string v, double w) {
-        
+		Edge newEdge;
+        newEdge.setSource(u);
+		newEdge.setTarget(v);
+		newEdge.setWeight(w);
     }
     
     
